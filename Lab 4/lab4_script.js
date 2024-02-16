01d05e0be79a88f93d0f6fdce047a0fa94224957
ben flynn <bflyn037@uottawa.ca>
2024-02-16 09:55:44 -0500

    Lab 4

--- Lab 4/lab4_script.js
+++ Lab 4/lab4_script.js
@@ -10,4 +10,52 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////// 11.7 /////////////////////////////////////////////////////////////////////////////
-// Questions: 11.7 & 11.14
+
+const articles = ["the", "a", "one", "some","any"]
+const nouns = ["boy", "girl", "dog", "town","car"]
+const verbs = ["drove", "jumped", "ran", "walked","skipped"]
+const prepositions = ["to", "from", "over", "under", "on"]
+const container = document.getElementById("sentence-container")
+const result = document.getElementById("result")
+
+
+function generateSentence() {
+    return "Once upon a time " + articles[Math.floor(Math.random() * articles.length)] + " " + nouns[Math.floor(Math.random() * nouns.length)] + " " + verbs[Math.floor(Math.random() * verbs.length)] + " " + prepositions[Math.floor(Math.random() * prepositions.length)] + " " + articles[Math.floor(Math.random() * articles.length)] + " " + nouns[Math.floor(Math.random() * nouns.length)] + " THE END.";
+}
+
+function clearSentences() {
+    container.innerHTML = "";
+}
+
+function printSentences() {
+    for(let i = 0; i < 20 ; i++) {
+        container.innerHTML += "<p>"+ generateSentence() + "</p>" + "<br>"
+    }
+}
+
+/////////////////////////////////////////////////////////////// 11.14 /////////////////////////////////////////////////////////////////////////////
+
+  function countLetterOccurrences() {
+    var text = document.getElementById("textInput").value.toLowerCase();
+    var letterCounts = new Array(26).fill(0);
+
+    for (var i = 0; i < text.length; i++) {
+      var charCode = text.charCodeAt(i);
+      if (charCode >= 97 && charCode <= 122) {
+        letterCounts[charCode - 97]++;
+      }
+    }
+
+    var output = "<table>";
+    output += "<tr><th>Letter</th><th>Occurrences</th></tr>";
+    for (var j = 0; j < 26; j++) {
+      output += "<tr><td>" + String.fromCharCode(j + 97) + "</td><td>" + letterCounts[j] + "</td></tr>";
+    }
+    output += "</table>";
+
+    result.innerHTML = output;
+  }
+
+  function clearTable() {
+    result.innerHTML = "";
+}
